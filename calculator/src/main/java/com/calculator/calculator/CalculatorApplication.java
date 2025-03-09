b4a7b086c8ef062735ed24d88c6097c22488568d
sw.yoon <zoomspeed77@gmail.com>
2025-03-09 15:52:55 +0900

    feat: add calculator app cli mode

--- calculator/src/main/java/com/calculator/calculator/CalculatorApplication.java
+++ calculator/src/main/java/com/calculator/calculator/CalculatorApplication.java
@@ -1,13 +1,22 @@
 package com.calculator.calculator;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CalculatorApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
+		if (args.length > 0 && args[0].equalsIgnoreCase("cli")) {
+			// CLI 모드로 실행 (Web 환경 비활성화)
+			SpringApplication app = new SpringApplication(CalculatorApplication.class);
+			app.setWebApplicationType(WebApplicationType.NONE);  // Web 서버 실행 방지
+			ConfigurableApplicationContext context = app.run(args);
+			context.close(); // CLI 실행 후 종료
+		} else {
+			// Web 애플리케이션 실행 (기본 모드)
+			SpringApplication.run(CalculatorApplication.class, args);
+		}
 	}
-
 }
